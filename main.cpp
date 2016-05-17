91d3355bf43f9cec5af37ec0aa19b09f11c96fd3
FirstTripla <firsttripla@gmail.com>
2016-05-17 12:57:44 +0200

    frissites

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main()
     //game_engine amoba(600,450,20);
     //game_engine amoba(1000,900,40);
         //mukodik vele, viszont a monitorom felbontasa csak 1366x768, de teszteltem ideiglenes gepen 1920x1080-n
-    game_engine amoba(800,600,30);
+    game_engine amoba(800,680,30);
     amoba.run();
 	return 0;
 }
